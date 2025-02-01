a32089c036d5514d5d2c1626283fc1f948fd75b6
daberpro <daber.coding@gmail.com>
2025-02-01 18:47:10 +0700

    update: fix import.meta.url undefined

--- netlify/functions/server.mjs
+++ netlify/functions/server.mjs
@@ -1,5 +1,5 @@
 
-import { SipelikanServer } from "../../src/server";
+import { SipelikanServer } from "../../src/server.mjs";
 import serverless from "serverless-http";
 
 export const handler = serverless(SipelikanServer);
